f8c478b69b8d68d2baf6de1dca601597aee11a15
tilteddepressed <tosltikov52@mail.ru>
2023-11-14 16:29:02 +0500

    fix simple problems

--- app/src/main/java/com/example/iqtest/datasource/ServiceBuilder.kt
+++ app/src/main/java/com/example/iqtest/datasource/ServiceBuilder.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://a374-79-172-44-238.ngrok.io/")
+        .baseUrl("https://d9d4-77-223-82-224.ngrok.io/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     fun <T> buildService(service: Class<T>): T {
